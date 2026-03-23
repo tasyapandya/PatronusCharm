eb83be9354499561b981e2c3bf1067b96491d792
Tasya Pandya <tasya.pandya@gmail.com>
2026-03-23 16:53:27 +0700

    Initial Commit

---
+++ PatronusCharm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PatronusCharm
+//
+//  Created by Tasya Pandya Latifa on 23/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PatronusCharm/PatronusCharmApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatronusCharmApp.swift
+//  PatronusCharm
+//
+//  Created by Tasya Pandya Latifa on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PatronusCharmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
